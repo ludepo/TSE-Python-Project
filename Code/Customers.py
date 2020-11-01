13d578f96ee7f6a9d3328aa6d4ac42fee7d41cc1
Camille Calandre <calandre.camille@hotmail.fr>
2020-11-01 22:51:45 +0100

    minor changes

--- Code/Customers.py
+++ Code/Customers.py
@@ -4,17 +4,18 @@ import pandas as pd
 import numpy as np
 import os
 
-importpath = os.path.abspath("./Results/dfprobs.csv")
+importpath = os.path.abspath("../Results/dfprobs.csv")
 dfprob = pd.read_csv(importpath, sep=";")
 
 dfprob.index = dfprob['ID']
 dfprob['HOUR'] = dfprob.ID.str.slice(stop=2)
-dfprob['MINUTE'] = dfprob.ID.str.slice(start = 3, stop=5)
+dfprob['MINUTE'] = dfprob.ID.str.slice(start=3, stop=5)
 
 prices = {'PRODUCT': ['coffee', 'frappucino', 'milkshake', 'soda', 'tea', 'water',
                       'cookie', 'muffin', 'pie', 'sandwich', 'nothing'],
           'PRICE': [3, 4, 5, 3, 3, 2, 2, 3, 3, 2, 0]}
 prices = pd.DataFrame(prices)
+
 
 class Customer(object):
     def __init__(self):
@@ -29,7 +30,7 @@ class Customer(object):
         beverages = []
         for i in range(len(self.purchases)):
             beverages.append(self.purchases[i].drink[0])
-        print("The customer %s has bought %s." %(self.ID, beverages)) # to improve: print not list but elements alone
+        print("The customer %s has bought %s." % (self.ID, beverages))  # to improve: print not list but elements alone
 
     def food_bought(self):
         snacks = []
@@ -42,13 +43,15 @@ class Customer(object):
         for i in range(len(self.purchases)):
             history.append([self.purchases[i].time, self.purchases[i].drink[0],
                             self.purchases[i].food[0], self.purchases[i].value])
-        print("The customer %s has made the following purchases: %s" %(self.ID, history))
+        print("The customer %s has made the following purchases: %s" % (self.ID, history))
+
 
 class Tripadvised(Customer):
     def __init__(self):
         super().__init__()
         self.type = "tripadvisor one time"
-        self.tip = random.choice(range(100, 1001))/100
+        self.tip = random.choice(range(100, 1001)) / 100
+
 
 class Returner(Customer):
     def __init__(self):
@@ -56,11 +59,13 @@ class Returner(Customer):
         self.type = "normal returning"
         self.budget = 250
 
+
 class Hipster(Customer):
     def __init__(self):
         super().__init__()
         self.type = "hipster returning"
         self.budget = 500
+
 
 class Purchase(object):
     def __init__(self, customer, hour, minute):
@@ -69,28 +74,29 @@ class Purchase(object):
 
         food = ['cookie', 'muffin', 'pie', 'sandwich', 'nothing']
         foodprob = dfprob[['FOOD_cookie', 'FOOD_muffin', 'FOOD_pie', 'FOOD_sandwich', 'FOOD_nothing', 'HOUR', 'MINUTE']]
-        foodprob = foodprob[(foodprob['HOUR'] == hour) & (foodprob['MINUTE'] == minute)].drop(['HOUR', 'MINUTE'], axis=1)
+        foodprob = foodprob[(foodprob['HOUR'] == hour) & (foodprob['MINUTE'] == minute)].drop(['HOUR', 'MINUTE'],
+                                                                                              axis=1)
         food = random.choices(food, weights=(foodprob.stack().tolist()), k=1)
 
         drink = ['coffee', 'frappucino', 'milkshake', 'soda', 'tea', 'water']
         drinkprob = dfprob[['DRINK_coffee', 'DRINK_frappucino', 'DRINK_milkshake', 'DRINK_soda',
                             'DRINK_tea', 'DRINK_water', 'HOUR', 'MINUTE']]
-        drinkprob = drinkprob[(drinkprob['HOUR'] == hour) & (drinkprob['MINUTE'] == minute)].drop(['HOUR', 'MINUTE'], axis=1)
+        drinkprob = drinkprob[(drinkprob['HOUR'] == hour) & (drinkprob['MINUTE'] == minute)].drop(['HOUR', 'MINUTE'],
+                                                                                                  axis=1)
         drink = random.choices(drink, weights=(drinkprob.stack().tolist()), k=1)
 
         self.drink = drink
         self.food = food
         self.value = prices[prices['PRODUCT'] == food[0]]['PRICE'].values[0] + \
                      prices[prices['PRODUCT'] == drink[0]]['PRICE'].values[0]
-        #self.payment = different from value as tips might be given
+        # self.payment = different from value as tips might be given
 
     def describe_purchase(self):
         print("The purchase of %s at %s:%s o'clock was a %s with %s to eat and had an overall value of %s€."
-              %(self.customer.ID, self.time[0], self.time[1], self.drink[0], self.food[0], self.value))
+              % (self.customer.ID, self.time[0], self.time[1], self.drink[0], self.food[0], self.value))
 
 
-
-###### Simulations file #################################################################################################
+###### Simulations file ###############################################################################################
 
 # Prepare dtataframe that will associate a purchase object to a customer object at given times
 transactions = dfprob[['HOUR', 'MINUTE']]
@@ -100,17 +106,21 @@ transactions['CUSTBUDG'] = ""
 transactions['MONEYSPENT'] = ""
 
 # Create list of returning customers
-ReturningCust = [Returner() for i in range(667)] # probability 2/3 for being normal returning customer (out of 1000 returning)
-ReturningCust.extend([Hipster() for i in range(333)]) # probability 1/3 for being hipster
+ReturningCust = [Returner() for i in
+                 range(667)]  # probability 2/3 for being normal returning customer (out of 1000 returning)
+ReturningCust.extend([Hipster() for i in range(333)])  # probability 1/3 for being hipster
+
 
 # Create function that defines what type of customer enters the cafe for a given time
 def ChooseCustomer(time):
-    liquid = [ReturningCust[i] for i in range(len(ReturningCust)) if ReturningCust[i].budget > 8] # is returner solvent?
-    returner = random.choice(liquid)                                         # define type of returner (normal/hipster)
+    liquid = [ReturningCust[i] for i in range(len(ReturningCust)) if
+              ReturningCust[i].budget > 8]  # is returner solvent?
+    returner = random.choice(liquid)  # define type of returner (normal/hipster)
     customer = random.choices([returner, Customer(), Tripadvised()],
-                              weights = [20, 72, 8], # 20% chance for returner, 72% normal one time customer,
-                              k = 1)                 #  8% tripadvisor customer
+                              weights=[20, 72, 8],  # 20% chance for returner, 72% normal one time customer,
+                              k=1)  # 8% tripadvisor customer
     return customer[0]
+
 
 # Assign type of customer per timeslot, find purchase object of the respective customer
 for i in range(0, len(transactions)):
@@ -120,27 +130,23 @@ for i in range(0, len(transactions)):
                                            transactions['HOUR'].values[i],
                                            transactions['MINUTE'].values[i])
     # update attributes of customer that did the purchase
-    transactions['CUSTOMER'][i].money_spent += transactions['PURCHASE'][i].value # change to payment once tips are done
+    transactions['CUSTOMER'][i].money_spent += transactions['PURCHASE'][i].value  # change to payment once tips are done
     transactions['CUSTOMER'][i].budget -= transactions['PURCHASE'][i].value
     transactions['CUSTOMER'][i].purchases.append(transactions['PURCHASE'][i])
 
 test = []
-for i in range(0,len(ReturningCust)):
+for i in range(0, len(ReturningCust)):
     test.append(ReturningCust[i].money_spent)
 
 test2 = []
-for i in range(0,len(ReturningCust)):
+for i in range(0, len(ReturningCust)):
     test2.append(ReturningCust[i].budget)
 
 test3 = []
-for i in range(0,len(ReturningCust)):
+for i in range(0, len(ReturningCust)):
     test3.append(ReturningCust[i].purchases)
 
 # Give examples of purchases
 print(transactions['PURCHASE'][0].describe_purchase(),
       transactions['PURCHASE'][1].describe_purchase(),
       transactions['PURCHASE'][2].describe_purchase())
-
-
-
-
--- Code/Exploratory.py
+++ Code/Exploratory.py
@@ -5,11 +5,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
-
-# define inupt and output path
-importpath = os.path.abspath("./Data/Coffeebar_2016-2020.csv")
-exportpath = os.path.abspath("./Results/dfprobs.csv")
+# define input and output path
+importpath = os.path.abspath("../Data/Coffeebar_2016-2020.csv")
+exportpath = os.path.abspath("../Results/dfprobs.csv")
 
 # load dataframe
 df = pd.read_csv(importpath, sep=";")
@@ -38,41 +36,38 @@ print(df.CUSTOMER.nunique())
 
 # 2) Create (at least) a bar plot of total amount of sold foods (plot1) and drinks (plot2) over the five years
 
-plt.bar(df.groupby(by="DRINKS",as_index=False).count().sort_values(by = 'TIME', ascending = False).DRINKS,
-        df.groupby(by="DRINKS",as_index=False).count().sort_values(by = 'TIME', ascending = False).TIME)
-#plt.show()
+plt.bar(df.groupby(by="DRINKS", as_index=False).count().sort_values(by='TIME', ascending=False).DRINKS,
+        df.groupby(by="DRINKS", as_index=False).count().sort_values(by='TIME', ascending=False).TIME)
+# plt.show()
 
-plt.bar(df.groupby(by="FOOD",as_index=False).count().sort_values(by = 'TIME', ascending = False).FOOD,
-        df.groupby(by="FOOD",as_index=False).count().sort_values(by = 'TIME', ascending = False).TIME)
-#plt.show()
+plt.bar(df.groupby(by="FOOD", as_index=False).count().sort_values(by='TIME', ascending=False).FOOD,
+        df.groupby(by="FOOD", as_index=False).count().sort_values(by='TIME', ascending=False).TIME)
+# plt.show()
 
 
 # 3) Determine the average that a customer buys a certain food or drink at any given time:
 dfprob = df.drop(['CUSTOMER', 'DATETIME', 'YEAR', 'WEEKDAY', 'DATE'], axis=1)
-dfprob = pd.get_dummies(dfprob, columns=["DRINKS", "FOOD"], prefix=["DRINK", "FOOD"]).\
-                 groupby('TIME').\
-                 mean()
+dfprob = pd.get_dummies(dfprob, columns=["DRINKS", "FOOD"], prefix=["DRINK", "FOOD"]). \
+    groupby('TIME'). \
+    mean()
 for i in dfprob.columns:
-    dfprob[i] = round(dfprob[i]*100)
+    dfprob[i] = round(dfprob[i] * 100)
 
 dfprob = dfprob.astype(int)
 dfprob['ID'] = dfprob.index
 
-for index,row in dfprob.iterrows():
+for index, row in dfprob.iterrows():
     print("On average the probabilities of a costumer buying the following drinks at %s are: "
           "coffee %s, soda %s, water %s, tea %s, milkshake %s, frappucino %s and for food: "
           "sandwich %s, muffin %s, cookie %s, pie %s or nothing %s."
-          %(row['ID'], row['DRINK_coffee'], row['DRINK_soda'], row['DRINK_water'], row['DRINK_tea'],
-            row['DRINK_milkshake'], row['DRINK_frappucino'],
-            row['FOOD_sandwich'], row['FOOD_muffin'], row['FOOD_cookie'], row['FOOD_pie'], row['FOOD_nothing']))
+          % (row['ID'], row['DRINK_coffee'], row['DRINK_soda'], row['DRINK_water'], row['DRINK_tea'],
+             row['DRINK_milkshake'], row['DRINK_frappucino'],
+             row['FOOD_sandwich'], row['FOOD_muffin'], row['FOOD_cookie'], row['FOOD_pie'], row['FOOD_nothing']))
 
-
-dfpropdrink= dfprob[['DRINK_coffee', 'DRINK_soda' , 'DRINK_frappucino', 'DRINK_milkshake', 'DRINK_tea', 'DRINK_water']]
+dfpropdrink = dfprob[['DRINK_coffee', 'DRINK_soda', 'DRINK_frappucino', 'DRINK_milkshake', 'DRINK_tea', 'DRINK_water']]
 dfpropdrink.plot.area()
 
-dfpropfood = dfprob [['FOOD_cookie', 'FOOD_muffin', 'FOOD_nothing', 'FOOD_pie', 'FOOD_sandwich']]
+dfpropfood = dfprob[['FOOD_cookie', 'FOOD_muffin', 'FOOD_nothing', 'FOOD_pie', 'FOOD_sandwich']]
 dfpropfood.plot.area()
 
-
-dfprob.to_csv(exportpath, sep=";", index = False)
-
+dfprob.to_csv(exportpath, sep=";", index=False)
