d60c4cf8bb473afb759a469a2652e16735f129db
Camille Calandre <calandre.camille@hotmail.fr>
2020-11-07 02:03:23 +0100

    prob for type of consumer

--- Code/Part 4.py
+++ Code/Part 4.py
@@ -3,8 +3,7 @@
 #2.From the provided datast:
 #how many returning cust?
 #specific time for returning?
- #   prob of 1 timer at a given time
-  #  prob of returning at a given time?
+ #   prob of 1 time ror returner at a given time?
    # how does this impact their buying history?
     #correlation?
 
@@ -34,21 +33,39 @@ df['DRINKS'].isnull().sum()
 df['FOOD'].isnull().sum()
 df = df.fillna('nothing')
 
-#Returning customer table
+#specific time for returning customers?
 returners=df[df['CUSTOMER'].duplicated(keep=False)]
-returners=returners.assign(pro=returners.TIME.map(returners.TIME.value_counts(normalize=True)))
-
+returners=returners.assign(prob_returners=returners.TIME.map(returners.TIME.value_counts(normalize=True)))
 returners.drop_duplicates(subset=['CUSTOMER'], keep=False)
-returners[['TIME','pro']].plot('TIME', figsize=(15,8))#graph for returners
+returners[['TIME','prob_returners']].plot('TIME', figsize=(15,8))#graph for returners
 
-#One-time customer
+#specific time for one-time customers?
 onetimer= df.drop_duplicates(subset= ['CUSTOMER'],keep=False)
-onetimer=onetimer.assign(pro=onetimer.TIME.map(onetimer.TIME.value_counts(normalize=True)))
-onetimer[['TIME','pro']].plot('TIME', figsize=(15,8)) #graph for onetime customers
+onetimer=onetimer.assign(prob_onetimer=onetimer.TIME.map(onetimer.TIME.value_counts(normalize=True)))
+onetimer[['TIME','prob_onetimer']].plot('TIME', figsize=(15,8)) #graph for onetime customers
 
 
 
 #
-df['RET']=(df.duplicated(keep=False, subset=['CUSTOMER']))*1
-df['RET'] = 'returner' if df['RET'] == 1 else 'onetimer'
-test=returners.groupby(['TIME', 'FOOD'], axis=1).count()
+df['RET']=(df.duplicated(keep=False, subset=['CUSTOMER']))*1 #dummy variable for returners
+
+time=df.groupby(['TIME', 'RET']).count()['CUSTOMER'].unstack(level=0) #count each type of customer at each period
+time=time.transpose()
+time= time.rename(columns={time.columns[0]: "onetimer",time.columns[1]:"returner"})
+time['prob_onetimer']=time['onetimer']/(time['onetimer']+time['returner'])
+time['prob_returner']=time['returner']/(time['onetimer']+time['returner']) # we have the probability at each given time that the consumer is onetimer of returner
+
+time.reset_index(inplace=True)
+time[['TIME','prob_onetimer']].plot('TIME', figsize=(15,8))#graph for onetimers
+time[['TIME','prob_returner']].plot('TIME', figsize=(15,8))#graph for returners
+
+#TBD: function to integrate those prob
+
+# graph test
+df.groupby(['TIME', 'DRINKS']).count()['YEAR'].unstack().plot()
+plt.show()
+
+
+#function for change in price
+
+#impact of: unlimited budget for returners? possibility of buying 2 drinks?
