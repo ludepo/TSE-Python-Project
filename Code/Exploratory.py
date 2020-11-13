813c838361f4619593eb115e2136f157528d608e
Camille Calandre <calandre.camille@hotmail.fr>
2020-11-13 22:53:56 +0100

    Reorganise the code structure

--- Code/Exploratory.py
+++ Code/Exploratory.py
@@ -1,26 +1,31 @@
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
-from numpy import cov
-
-
+import numpy as np
 
 ## *********************************************************************************************************************
-## I: Define Inputs and prepare data  **********************************************************************************
+## 0: Import data ******************************************************************************************************
 ## *********************************************************************************************************************
+
 
 # define input and output path
 importpath = os.path.abspath("./Data/Coffeebar_2016-2020.csv")
-exportpath = os.path.abspath("./Results/dfprobs.csv")
+exportpath_probs = os.path.abspath("./Results/dfprobs.csv")
+exportpath_data = os.path.abspath("./Results/coffee_bar_prices.csv")
 
 # load dataframe
 df = pd.read_csv(importpath, sep=";")
 
-# explore data
+## *********************************************************************************************************************
+## I: Transformation of the Given Dataset ******************************************************************************
+## *********************************************************************************************************************
+
+# 1.1) explore data
 print(df.head())
 print(df.dtypes)
 
-# add variables for analysis
+
+# 1.2) Add variables for analysis
 def addcolumns(dataframe):  # function serves to add variables for easier grouping of data
     data = dataframe.copy(deep=True)  # Make a copy so dataframe not overwritten
     data['DATETIME'] = pd.to_datetime(data['TIME'])
@@ -34,14 +39,11 @@ def addcolumns(dataframe):  # function serves to add variables for easier groupi
 
 df = addcolumns(df)
 
-
-
-
 ## *********************************************************************************************************************
 ## II: Insight of the data  ********************************************************************************************
 ## *********************************************************************************************************************
 
-# 1) What food and drinks are sold by the coffee bar?
+# 2.1) What food and drinks are sold by the coffee bar?
 print(df.DRINKS.value_counts())
 print(df.FOOD.value_counts())
 
@@ -49,13 +51,13 @@ print(df.FOOD.value_counts())
 print(df.CUSTOMER.count())
 print(df.CUSTOMER.nunique())
 
-
-# 2) Create (at least) a bar plot of total amount of sold foods (plot1) and drinks (plot2) over the five years
-# -- Count number of each food/drik sold over the 5 years span
+# 2.2) Create plots to illustrate the data
+# -- Count number of each drinks sold over the 5 years span
 plt.bar(df.groupby(by="DRINKS", as_index=False).count().sort_values(by='TIME', ascending=False).DRINKS,
         df.groupby(by="DRINKS", as_index=False).count().sort_values(by='TIME', ascending=False).TIME)
 plt.show()
 
+# -- Count number of each food sold over the 5 years span
 plt.bar(df.groupby(by="FOOD", as_index=False).count().sort_values(by='TIME', ascending=False).FOOD,
         df.groupby(by="FOOD", as_index=False).count().sort_values(by='TIME', ascending=False).TIME)
 plt.show()
@@ -71,9 +73,6 @@ df.groupby(['WEEKDAY', 'DRINKS']).count()['YEAR'].unstack().plot.bar()
 plt.show()
 df.groupby(['WEEKDAY', 'FOOD']).count()['YEAR'].unstack().plot.bar()
 plt.show()
-
-
-
 
 ## *********************************************************************************************************************
 ## III: Obtain time probabilities   ************************************************************************************
@@ -109,4 +108,59 @@ dfpropfood = dfprob[['FOOD_cookie', 'FOOD_muffin', 'FOOD_nothing', 'FOOD_pie', '
 dfpropfood.plot.area()
 plt.show()
 
-dfprob.to_csv(exportpath, sep=";", index=False)
+## *********************************************************************************************************************
+## IV: Add prices for later comparison *********************************************************************************
+## *********************************************************************************************************************
+
+# 4.1) Assign price to each item
+prices_drinks = {'DRINKS': ['coffee', 'frappucino', 'milkshake', 'soda', 'tea', 'water'],
+                 'PRICE_DRINKS': [3, 4, 5, 3, 3, 2]}
+prices_drinks = pd.DataFrame(prices_drinks)
+
+prices_food = {'FOOD': ['cookie', 'muffin', 'pie', 'sandwich', 'nothing'],
+               'PRICE_FOOD': [2, 3, 3, 2, 0]}
+prices_food = pd.DataFrame(prices_food)
+
+
+# 4.2) Add turnover in the data
+# function for turnover
+def prices(dataframe):
+    dataframe = pd.merge(dataframe, prices_drinks, how='left', on='DRINKS')
+    dataframe = pd.merge(dataframe, prices_food, how='left', on='FOOD')
+    dataframe['TURNOVER'] = dataframe['PRICE_FOOD'] + dataframe['PRICE_DRINKS']
+    return dataframe
+
+
+df_prices = prices(df)
+
+
+# 4.2) Add tips in the data
+# function for tips : we don't know who are the tripadvised customers in this dataset, only that they represent 10% of
+# one-time customers. So we randomly select the 10% of the one-time customers that will pay a tip.
+def tips(dataframe):  # function that takes a subsample of 10% of onetimers and assign them a tip
+    dataframe = dataframe.drop_duplicates(subset=['CUSTOMER'], keep=False)
+    dataframe = dataframe.sample(frac=.1)
+    dataframe['TIPS'] = (np.random.randint(0, 11, size=len(dataframe)))
+    dataframe = dataframe[['CUSTOMER', 'TIPS']]
+    dataframe['TIPS'] = dataframe['TIPS'].astype(int)
+    return dataframe
+
+
+df_tips = tips(df)
+
+
+def total(df_prices):  # function that merge turnover and tips in the same dataset
+    df_prices = pd.merge(df_prices, df_tips, how='left', on='CUSTOMER')
+    df_prices['TIPS'] = df_prices['TIPS'].fillna(0)
+    return df_prices
+
+
+coffee_bar_prices = total(df_prices)
+
+## *********************************************************************************************************************
+## V: Export Data ******************************************************************************************************
+## *********************************************************************************************************************
+# Export database that will be used later
+
+dfprob.to_csv(exportpath_probs, sep=";", index=False)
+coffee_bar_prices.to_csv(exportpath_data, sep=";", index=False)
