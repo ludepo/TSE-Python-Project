921c96bf741c3e90275bccf43c39c0f860f77d09
Luca Poll <lupoll208@gmail.com>
2020-11-15 18:12:38 +0100

    all works, final commit

--- Code/Simulation.py
+++ Code/Simulation.py
@@ -1,7 +1,6 @@
 from Code.Customers import *
 import pickle
 import matplotlib.pyplot as plt
-import numpy as np
 import os
 
 ## *********************************************************************************************************************
